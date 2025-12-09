5f568d84167a5e828e8212b831d8594d75fd58db
janis <janis.akmenskalns@gmail.com>
2025-12-09 15:55:30 +0200

    add on_pointing_axis_tracking

--- test_side_lobes.py
+++ test_side_lobes.py
@@ -183,7 +183,7 @@ def main(station, rcumode, subband_min,  subband_max,  target_source, start_time
     print("samptimes", samptimes[0], samptimes[1], len(samptimes), len(times))
     print("freqs_joins", freqs_joins[0], freqs_joins[1], freqs_joins[2], freqs_joins[-1], len(freqs_joins), len(freqs))
     print("jonesobj", jonesobj)
-    print("jones", jones.shape, a_team_sum.shape)
+    print("jones", jones.shape, a_team_sum.shape, jones[:, :, 0, 0].shape)
 
     sys.exit(0)
 
